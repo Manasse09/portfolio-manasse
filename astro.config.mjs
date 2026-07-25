04ab75771d35dc9bf11df0de94e6e8293e9d2014
Manasse09 <manassegilberto@gmail.com>
2026-07-25 16:27:20 +0300

    Mon portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  // 1. Remplace par ton pseudo GitHub
+  site: "https://manasse09.github.io",
+
+  // 2. À configurer selon le nom de ton dépôt GitHub :
+  // - Si ton dépôt s'appelle "portfolio" -> met : base: "/portfolio",
+  // - Si ton dépôt s'appelle "<ton-pseudo>.github.io" -> efface simplement cette ligne "base"
+  base: "/portfolio-manasse",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
