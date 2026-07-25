e7f1447a1b91e8c7d340ec41ec6b0061375c51b8
Manasse09 <manassegilberto@gmail.com>
2026-07-25 17:34:56 +0300

    Mise à jour de la configuration et des composants

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,8 @@ export default defineConfig({
   // 2. À configurer selon le nom de ton dépôt GitHub :
   // - Si ton dépôt s'appelle "portfolio" -> met : base: "/portfolio",
   // - Si ton dépôt s'appelle "<ton-pseudo>.github.io" -> efface simplement cette ligne "base"
-  base: "/portfolio-manasse",
-
+  base: "portfolio-manasse",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
